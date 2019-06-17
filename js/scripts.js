c2b57cbf23d6dc51b5f4207758f8a99739a56210
faithkoske <chemutaifaith10@gmail.com>
2019-06-17 15:33:38 +0300

    add file changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,4 @@
-function Cost(sizes,toppings,crust,delivery){
-    this.sizes=sizes;
-    this.crust=crust;
-    this.number=number;
-    this.toppings=toppings;
-}
-function sizes(large,medium,small){
-    this.large=2500;
-    this.medium=1500;
-    this.small=1000;
-}
-function crust(thin,stuffed,cheesefilled){
-    this.thin=500;
-    this.stuffed=700;
-    this.cheesefilled=900;
-}
-function toppings(pepperonni,mushrooms,onions,sausage ,olives){
-    this.pepperonni=200;
-    this.onions=200;
-    this.mushrooms=200;
-    this.olives=200;
-    this.sausage=200;
-}
-function delivery(Yes,No){
-    this.Yes=500;
-    this.No=0;
-}
-Cost.prototype.cost = function() {
-    return this.sizes + this.crust+this.toppings+ this.delivery;
-}
+
 
 //user interface
 function loc(){
